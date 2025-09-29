0b54ef632fa97c9ee28e5cf91c66c215b25582d2
Wouter Lievens <lievenswouter@gmail.com>
2025-09-29 22:40:57 +0200

    stub person page

--- src/app/pages/person-page/person-page.ts
+++ src/app/pages/person-page/person-page.ts
@@ -1,11 +1,35 @@
+import {AsyncPipe} from '@angular/common';
 import {Component} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {GamePersonService, PersonGameFull} from '../../../api';
+import {LoadedObject} from '../../classes/loaded-object';
+import {PortraitUrlPipe} from '../../pipes/portrait-url-pipe';
 
 @Component({
   selector: 'pt-person-page',
-  imports: [],
+  imports: [
+    AsyncPipe,
+    PortraitUrlPipe,
+  ],
   templateUrl: './person-page.html',
   styleUrl: './person-page.scss'
 })
 export class PersonPage {
+  worldId?: string;
+  person$?: LoadedObject<PersonGameFull>;
 
+  constructor(
+    private route: ActivatedRoute,
+    private personService: GamePersonService,
+  ) {
+  }
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      const worldId = params['worldId'];
+      const personId = params['personId'];
+      this.worldId = worldId;
+      this.person$ = new LoadedObject<PersonGameFull>(() => this.personService.gameGetPersonById(worldId, personId));
+    });
+  }
 }
