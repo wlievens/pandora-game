73ec13236132a24dc32e4e1a9f067fed8179d9f6
Wouter Lievens <lievenswouter@gmail.com>
2025-09-30 00:07:27 +0200

    party stub page

--- src/app/pages/party-page/party-page.ts
+++ src/app/pages/party-page/party-page.ts
@@ -1,13 +1,18 @@
 import {AsyncPipe} from '@angular/common';
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, Params} from '@angular/router';
+import {ActivatedRoute, Params, RouterLink} from '@angular/router';
 import {GamePartyService, PartyGameFull} from '../../../api';
 import {LoadedObject} from '../../classes/loaded-object';
+import {Compass} from '../../components/compass/compass';
+import {GovernmentFlagUrlPipe} from '../../pipes/government-flag-url-pipe';
 
 @Component({
   selector: 'pt-party-page',
   imports: [
-    AsyncPipe
+    AsyncPipe,
+    Compass,
+    GovernmentFlagUrlPipe,
+    RouterLink,
   ],
   templateUrl: './party-page.html',
   styleUrl: './party-page.scss'
